5550222608fe1a57b94ca4b3525603ac6a36f08c
ernes <ernes@DESKTOP-N4EHHNH>
2020-06-29 10:02:17 -0500

    Programa funcionando con la clase ZonaEntity

---
+++ cfe-backend-apirest/src/main/java/com/ablevm/cfe/backend/apirest/CfeBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.ablevm.cfe.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfeBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfeBackendApirestApplication.class, args);
+	}
+
+}
---
+++ cfe-backend-apirest/src/test/java/com/ablevm/cfe/backend/apirest/CfeBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ablevm.cfe.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfeBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
