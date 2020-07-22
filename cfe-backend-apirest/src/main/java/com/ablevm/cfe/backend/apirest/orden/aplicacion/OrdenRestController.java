8dbe61805b05bc9955b81711d6467189f5f25998
Alberto Chio <a.chio@outlook.com>
2020-07-22 17:10:55 -0500

    CrossOrigin

--- cfe-backend-apirest/src/main/java/com/ablevm/cfe/backend/apirest/orden/aplicacion/OrdenRestController.java
+++ cfe-backend-apirest/src/main/java/com/ablevm/cfe/backend/apirest/orden/aplicacion/OrdenRestController.java
@@ -3,6 +3,7 @@ package com.ablevm.cfe.backend.apirest.orden.aplicacion;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ablevm.cfe.backend.apirest.orden.dominio.OrdenEntity;
 import com.ablevm.cfe.backend.apirest.orden.infraestructura.OrdenService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class OrdenRestController {
